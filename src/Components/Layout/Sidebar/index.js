e1e491ef1b2ce2d22c7a3c365241c90398c7f613
sigmatech360 <syed.danial@sigmatechservices.net>
2025-03-18 23:54:13 +0500

    sidebar

--- src/Components/Layout/Sidebar/index.js
+++ src/Components/Layout/Sidebar/index.js
@@ -28,210 +28,41 @@ import {
 
 import "./style.css";
 
+const sideBarNavlinks = [
+  { title: 'Dashboard', link: '/dashboard', icon: Dashbord },
+  { title: 'Volunteers Management', link: '/volunteer-management', icon: VolunteersManagement },
+  { title: 'Create Notification', link: '/create-notification', icon: VolunteersManagement },
+  { title: 'Programs Management', link: '/programs-management', icon: ProgramsManagement },
+  { title: 'Event Management', link: '/event-management', icon: EventManagement },
+  { title: 'Podcast Management', link: '/podcast-management', icon: PodcastManagement },
+  { title: 'Sponsor Program', link: '/sponsor-program', icon: SponsorshipManagement },
+  { title: 'Top Volunteer Management', link: '/top-volunteer-management', icon: VolunteersManagement },
+  { title: 'Announcement Management', link: '/announcement-management', icon: AnnouncementManagement },
+  { title: 'Sponsorship Management', link: '/sponsorship-management', icon: SponsorshipManagement },
+  { title: 'Our Works Management', link: '/project-management', icon: Dashbord },
+  { title: 'Brand Management', link: '/brand-management', icon: BrandManagement },
+  { title: 'Member Management', link: '/member-management', icon: MemberManagement }
+];
+
 export const Sidebar = (props) => {
 
   const location = useLocation()
   return (
     <div className={`sidebar ${props.sideClass}`} id="sidebar">
       <ul className="list-unstyled">
-
-
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/dashboard') ? 'active' : ''}`} to="/dashboard">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faBorderAll} /> */}
-              <img src={Dashbord} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Dashboard</span>
-          </Link>
-        </li>
-
-
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/volunteer-management') ? 'active' : ''}`} to="/volunteer-management">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faMessage} /> */}
-              <img src={VolunteersManagement} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Volunteers Management</span>
-          </Link>
-        </li>
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/create-notification') ? 'active' : ''}`} to="/create-notification">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faMessage} /> */}
-              <img src={VolunteersManagement} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Create Notification</span>
-          </Link>
-        </li>
-
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/programs-management') ? 'active' : ''}`} to="/programs-management">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faMessage} /> */}
-              <img src={ProgramsManagement} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Programs Management</span>
-          </Link>
-        </li>
-
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/event-management') ? 'active' : ''}`} to="/event-management">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faEye} /> */}
-              <img src={EventManagement} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Event Management</span>
-          </Link>
-        </li>
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/podcast-management') ? 'active' : ''}`} to="/podcast-management">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faTasks} /> */}
-              <img src={PodcastManagement} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Podcast Management</span>
-          </Link>
-        </li>
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/sponsor-program') ? 'active' : ''}`} to="/sponsor-program">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faTasks} /> */}
-              <img src={SponsorshipManagement} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Sponsor Program</span>
-          </Link>
-        </li>
-
-
-
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/top-volunteer-management') ? 'active' : ''}`} to="/top-volunteer-management">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faTasks} /> */}
-              <img src={VolunteersManagement} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Top Volunteer Management</span>
-          </Link>
-        </li>
-
-
-
-         
-
-
-
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/announcement-management') ? 'active' : ''}`} to="/announcement-management">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faMountainCity} /> */}
-              <img src={AnnouncementManagement} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Announcement Management</span>
-          </Link>
-        </li>
-
-        {/* <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/customer-support') ? 'active' : ''}`} to="/customer-support">
-            <span className="sideIcon">
-              <FontAwesomeIcon icon={faHeadset} />
-            </span>
-            <span className="sideLinkText">Customer Support</span>
-          </Link>
-        </li> */}
-
-        {/* <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/currency-management') ? 'active' : ''}`} to="/currency-management">
-            <span className="sideIcon">
-              <FontAwesomeIcon icon={faMoneyBill} />
-            </span>
-            <span className="sideLinkText">Leadership Management</span>
-          </Link>
-        </li>
-        
-        */}
-
-
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/sponsorship-management') ? 'active' : ''}`} to="/sponsorship-management">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faUser} /> */}
-              <img src={SponsorshipManagement} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Sponsorship   Management</span>
-          </Link>
-        </li>
-
-
-        {/* <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/notification-list') ? 'active' : ''}`} to="/notification-list">
-            <span className="sideIcon">
-              <FontAwesomeIcon icon={faEye} />
-            </span>
-            <span className="sideLinkText"> Notification Management</span>
-          </Link>
-        </li> */}
-
-
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/project-management') ? 'active' : ''}`} to="/project-management">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faEye} /> */}
-              <img src={Dashbord} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">
-              
-              
-              
-            Our Works    Management</span>
-          </Link>
-        </li>
-
-
-
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/brand-management') ? 'active' : ''}`} to="/brand-management">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faEye} /> */}
-              <img src={BrandManagement} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Brand  Management</span>
-          </Link>
-        </li>
-
-
-
-
-
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/member-management') ? 'active' : ''}`} to="/member-management">
-            <span className="sideIcon">
-              {/* <FontAwesomeIcon icon={faEye} /> */}
-              <img src={MemberManagement} alt="Side Icon" />
-            </span>
-            <span className="sideLinkText">Member  Management</span>
-          </Link>
-        </li>
-
-
-        {/* 
-        <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/user-management') ? 'active' : ''}`} to="/user-management">
-            <span className="sideIcon">
-              <FontAwesomeIcon icon={faUser} />
-            </span>
-            <span className="sideLinkText">User  Management</span>
-          </Link>
-        </li> */}
-        {/* <li className="sidebar-li">
-          <Link className={`sideLink ${location.pathname.includes('/target-listing') ? 'active' : ''}`} to="/target-listing">
-            <span className="sideIcon">
-              <FontAwesomeIcon icon={faMoneyBill} />
-            </span>
-            <span className="sideLinkText">Project</span>
-          </Link>
-        </li> */}
+        {sideBarNavlinks.map((linkItem) => (
+          <li className="sidebar-li" key={linkItem.link}>
+            <Link
+              className={`sideLink ${location.pathname.includes(linkItem.link) ? 'active' : ''}`}
+              to={linkItem.link}
+            >
+              <span className="sideIcon">
+                <img src={linkItem.icon} alt="Side Icon" />
+              </span>
+              <span className="sideLinkText">{linkItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
